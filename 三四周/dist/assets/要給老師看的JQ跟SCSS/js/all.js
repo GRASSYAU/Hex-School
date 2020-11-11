871fb4d444bdbde29fab4d36443a262ad1b3a5c6
grassyau <kj25940858@gmail.com>
2020-11-12 03:13:34 +0800

    gulp

--- 三四周/dist/assets/要給老師看的JQ跟SCSS/js/all.js
+++ 三四周/dist/assets/要給老師看的JQ跟SCSS/js/all.js
@@ -1,14 +1,12 @@
-"use strict";
-
 $(".series-nav li").eq(0).addClass("li-hover");
 $(".series-glasses").eq(0).addClass("open");
-$(".series-nav li").click(function (e) {
+
+$(".series-nav li").click(function (e) { 
   e.preventDefault();
   $(".series-glasses").removeClass("open");
   var now = $(this).index();
   $(".series-glasses").eq(now).addClass("open");
   $(".series-nav li").removeClass("li-hover");
   $(".series-nav li").eq(now).addClass("li-hover");
-  console.log(now);
+  console.log(now)
 });
-//# sourceMappingURL=all.js.map
