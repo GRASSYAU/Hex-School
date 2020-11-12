dc08a8d2fa4d964db1363fe5139698d6ea07a8cd
grassyau <kj25940858@gmail.com>
2020-11-12 11:30:50 +0800

    11/12

--- 三四周/dist/assets/js/all.js
+++ 三四周/dist/assets/js/all.js
@@ -1,16 +1,5 @@
 "use strict";
 
-// $(".series-nav li").eq(0).addClass("li-hover");
-// $(".series-glasses").eq(0).addClass("open");
-// $(".series-nav li").click(function (e) { 
-//   e.preventDefault();
-//   $(".series-glasses").removeClass("open");
-//   var now = $(this).index();
-//   $(".series-glasses").eq(now).addClass("open");
-//   $(".series-nav li").removeClass("li-hover");
-//   $(".series-nav li").eq(now).addClass("li-hover");
-//   console.log(now)
-// });
 $(".series-nav li").eq(0).addClass("li-hover");
 $(".all .a").eq(0).addClass("open");
 $(".series-nav li").click(function (e) {
