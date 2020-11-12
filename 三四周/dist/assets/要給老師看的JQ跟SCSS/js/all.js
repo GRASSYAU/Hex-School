64cf752cc923c79a176e04e6ba3a8237364f15ed
grassyau <kj25940858@gmail.com>
2020-11-12 11:26:13 +0800

    Gule11/12

--- 三四周/dist/assets/要給老師看的JQ跟SCSS/js/all.js
+++ 三四周/dist/assets/要給老師看的JQ跟SCSS/js/all.js
@@ -1,11 +1,23 @@
-$(".series-nav li").eq(0).addClass("li-hover");
-$(".series-glasses").eq(0).addClass("open");
+// $(".series-nav li").eq(0).addClass("li-hover");
+// $(".series-glasses").eq(0).addClass("open");
 
+// $(".series-nav li").click(function (e) { 
+//   e.preventDefault();
+//   $(".series-glasses").removeClass("open");
+//   var now = $(this).index();
+//   $(".series-glasses").eq(now).addClass("open");
+//   $(".series-nav li").removeClass("li-hover");
+//   $(".series-nav li").eq(now).addClass("li-hover");
+//   console.log(now)
+// });
+
+$(".series-nav li").eq(0).addClass("li-hover");
+$(".all .a").eq(0).addClass("open");
 $(".series-nav li").click(function (e) { 
   e.preventDefault();
-  $(".series-glasses").removeClass("open");
+  $(".all .a").removeClass("open");
   var now = $(this).index();
-  $(".series-glasses").eq(now).addClass("open");
+  $(".all .a").eq(now).addClass("open");
   $(".series-nav li").removeClass("li-hover");
   $(".series-nav li").eq(now).addClass("li-hover");
   console.log(now)
