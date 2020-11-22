dc23e2d34dfcd199c2f96ef739a59991754bb2d0
grassyau <kj25940858@gmail.com>
2020-11-22 23:00:52 +0800

    11/22

--- 五周/dist/assets/js/all.js
+++ 五周/dist/assets/js/all.js
@@ -23,12 +23,19 @@ $("#btn-Cancel").click(function (e) {
   $(".abc").slideDown();
   $(".xxx").addClass("animate__animated animate__lightSpeedInRight").hide();
 }); // nav-active
+// $(".menu li").find("a").eq(2).addClass("nav-active")
+// $(".menu li").click(function (e) {
+//   var now = $(this).index();
+//   $(".menu li").find("a").removeClass("nav-active")
+//   $(".menu li").find("a").eq(now).addClass("nav-active");
+//   console.log(now)
+// })
 
-$(".menu li").find("a").eq(2).addClass("nav-active");
-$(".menu li").click(function (e) {
-  var now = $(this).index();
-  $(".menu li").find("a").removeClass("nav-active");
-  $(".menu li").find("a").eq(now).addClass("nav-active");
-  console.log(now);
+var url = window.location.pathname;
+$('.menu li').find('a').each(function () {
+  $(this).toggleClass('nav-active', $(this).attr('href') == url.split('/')[1]);
+});
+$("#qwe").click(function () {
+  $(".toggle-admin").stop(true, false).slideToggle();
 });
 //# sourceMappingURL=all.js.map
